5395ff36eff6c8ad3f4f5c100078c65163a3f2a0
aleksei.zolotykh <aazolotyh@gmail.com>
2024-07-19 04:00:51 +0300

    update task

--- dev/tasks.clj
+++ dev/tasks.clj
@@ -1,14 +1,16 @@
 (ns tasks
-  (:require [com.biffweb.tasks :as tasks]))
+  (:require
+   [clojure.test :refer [run-all-tests]]
+   [com.biffweb.tasks :as tasks]))
 
-(defn hello
-  "Says 'Hello'"
+(defn test
+  "run all unit test"
   []
-  (println "Hello"))
+  (run-all-tests))
 
 ;; Tasks should be vars (#'hello instead of hello) so that `clj -M:dev help` can
 ;; print their docstrings.
 (def custom-tasks
-  {"hello" #'hello})
+  {"test" #'test})
 
 (def tasks (merge tasks/tasks custom-tasks))
