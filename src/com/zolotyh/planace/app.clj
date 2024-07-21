626e4d9fb0abc6ed283dc19f563b493f2d36f93e
aleksei.zolotykh <aazolotyh@gmail.com>
2024-07-21 04:49:33 +0300

    remove debug information

--- src/com/zolotyh/planace/app.clj
+++ src/com/zolotyh/planace/app.clj
@@ -102,13 +102,11 @@
 
 (defn render-room [{:keys [path-params biff/db]}]
   (let [room (xt/entity db (parse-uuid (:room-id path-params)))
-        vote (xt/entity db (:room/active-vote room))
-        vote-1 (lookup-all db :vote/title "Vote 1")]
+        vote (xt/entity db (:room/active-vote room))]
     (ui/page
      {:base/title (str (:token path-params))}
      (voter-list voters-demo-list)
      [:div "vote: " (:vote/title vote) "active vote id: " (:room/active-vote room)]
-     (map (fn [n] [:div (:xt/id n) " vote " (:vote/title n)]) vote-1)
      [:p "room: " (:room/name room)])))
 
 (def module
