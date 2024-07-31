e7402f5cebec44ba815b1002533b13a254dc2205
aleksei.zolotykh <aazolotyh@gmail.com>
2024-07-31 23:37:24 +0300

    add open close param to schema and fixture

--- src/com/zolotyh/planace/schema.clj
+++ src/com/zolotyh/planace/schema.clj
@@ -23,6 +23,7 @@
    :vote/id :uuid
    :vote [:map {:closed false}
           [:xt/id       :vote/id]
+          [:vote/open?  :boolean]
           [:vote/owner    :user/id]
           [:vote/results [:map-of :user/id [:map {:closed true}
                                             [:vote int?]
