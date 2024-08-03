d2377e423d226118898607e59937c8e695d4fa58
aleksei.zolotykh <aazolotyh@gmail.com>
2024-08-03 16:05:33 +0300

    add fixtures for room with users

--- src/com/zolotyh/planace/schema.clj
+++ src/com/zolotyh/planace/schema.clj
@@ -1,22 +1,38 @@
 (ns com.zolotyh.planace.schema)
 
+
 (def schema
   {:user/id :uuid,
-   :user [:map {:closed true} [:xt/id :user/id] [:user/email :string]
+   :user [:map {:closed true}
+          [:xt/id :user/id]
+          [:user/email :string]
           [:user/joined-at inst?] [:user/first-name {:optional true} :string]
           [:user/last-name {:optional true} :string]
           [:user/foo {:optional true} :string]
           [:user/bar {:optional true} :string]],
    :room/id :uuid,
-   :room [:map {:closed true} [:xt/id :room/id] [:room/user :user/id]
-          [:room/active-vote :vote/id] [:room/name :string]
-          [:room/created-at inst?] [:room/update-at inst?]],
+   :room [:map {:closed true}
+          [:xt/id :room/id]
+          [:room/user :user/id]
+          [:room/active-vote :vote/id]
+          [:room/name :string]
+          [:room/created-at inst?]
+          [:room/users [:vector {:optional true}
+                        [:map {:closed true}
+                         [:id :uuid]
+                         [:first-name string?]
+                         [:last-name string?]]]]
+          [:room/update-at inst?]],
+
    :vote/id :uuid,
-   :vote [:map {:closed false} [:xt/id :vote/id] [:vote/open? :boolean]
+   :vote [:map {:closed false}
+          [:xt/id :vote/id]
+          [:vote/open? :boolean]
           [:vote/owner :user/id]
           [:vote/results
            [:map-of :user/id
-            [:map {:closed true} [:vote int?]
+            [:map {:closed true}
+             [:vote int?]
              [:first-name {:optional true} :string]
              [:last-name {:optional true} :string]]]]
           [:vote/room :room/id]
