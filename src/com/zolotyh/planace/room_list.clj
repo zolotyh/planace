dcc22fbfbcf5f6184b199471c917bb686d6b87d9
aleksei.zolotykh <aazolotyh@gmail.com>
2024-10-05 01:56:57 +0300

    intermid commit

--- src/com/zolotyh/planace/room_list.clj
+++ src/com/zolotyh/planace/room_list.clj
@@ -8,25 +8,27 @@
 (def spinner-id "spinner")
 (def param "s")
 
+(defn random-rooms []
+  (map (fn [i] {:id (random-uuid) :title (str "item" i)}) (range (rand-int 10))))
+
 (defn search [{:keys [query-params]}]
   [:div
    [:input {:value (query-params param)
             :type "text"
             :name "s"
+            :autocomplete "off"
             :hx-params "*"
             :hx-indicator (str "#" spinner-id)
             :hx-get "/room-list/search"
-            :hx-trigger "input changed delay:500ms, search"
+            :hx-trigger "input changed delay:300ms, search"
             :hx-target (str "#" id)
             :placeholder "Search"}]])
 
-
-
 (defn room [_ctx, value]
-  [:div (str "room" value)])
+  [:div (str "room" (:title value))])
 
 (defn room-list [ctx]
-  (let [items (range (rand-int 10))]
+  (let [items (random-rooms)]
     [:<>
      [:div {:id id}
       [:div {:id spinner-id :class "loader"}]
