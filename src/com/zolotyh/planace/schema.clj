e9c09c1a517f9eab73335e8d9808b01e8f4aaec5
aleksei.zolotykh <aazolotyh@gmail.com>
2025-01-09 00:25:05 +0300

    update schema

--- src/com/zolotyh/planace/schema.clj
+++ src/com/zolotyh/planace/schema.clj
@@ -9,6 +9,20 @@
           [:user/foo {:optional true} :string]
           [:user/bar {:optional true} :string]]
 
+   :room/id :uuid
+   :room [:map {:closed true}
+          [:xt/id       :room/id]
+          [:room/owner    :user/id]
+          [:room/title    :string]
+          [:room/created-at inst?]]
+
+   :vote/id :uuid
+   :vote [:map {:closed true}
+          [:xt/id       :vote/id]
+          [:vote/room    :room/id]
+          [:vote/closed?    :room/id]
+          [:vote/created-at inst?]]
+
    :msg/id :uuid
    :msg [:map {:closed true}
          [:xt/id       :msg/id]
