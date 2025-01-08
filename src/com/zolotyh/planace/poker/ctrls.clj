ca5a8e79844d53af135a02fe953440753a8bf8b6
aleksei.zolotykh <aazolotyh@gmail.com>
2025-01-09 00:09:34 +0300

    ui changes

--- src/com/zolotyh/planace/poker/ctrls.clj
+++ src/com/zolotyh/planace/poker/ctrls.clj
@@ -1,4 +1,17 @@
-(ns com.zolotyh.planace.poker.ctrls)
+(ns com.zolotyh.planace.poker.ctrls
+  (:require
+   [com.zolotyh.planace.poker.ui :as ui]))
+
+(def test-data {:votes
+                (->>
+                 (range 2)
+                 (map #(hash-map :val % :user (random-uuid))))
+                :options
+                (->>
+                 (range 3)
+                 (map #(hash-map :val % :keys %)))
+                :room {:title "<>room title<> "
+                       :closed? true}})
 
 (defn room [_]
-  [:div "room"])
+  (ui/room test-data))
--- src/com/zolotyh/planace/poker/ui.clj
+++ src/com/zolotyh/planace/poker/ui.clj
@@ -3,25 +3,24 @@
    [cheshire.core :as cheshire]
    [com.zolotyh.planace.poker.ids :as ids]))
 
+(defn results-item [item]
+  [:li
+   (cheshire/generate-string  item {:pretty true})])
+
 (defn results [votes]
-  [:div (cheshire/generate-string  votes {:pretty true})])
+  [:div (map results-item votes)])
+
+(defn voting-item [item]
+  [:li (cheshire/generate-string item {:pretty true})])
 
 (defn voting [options]
-  [:div (cheshire/generate-string  options {:pretty true})])
+  [:div (map voting-item options)])
 
 (defn room [{:keys [votes options room]}]
-  (let [{:keys [title]} room]
+  (let [{:keys [title closed?]} room]
     [:div {:id ids/room}
-     title
+     [:h1
+      title]
+     [:h3 (if closed? true false)]
      (voting options)
      (results votes)]))
-
-(room {:votes
-       (->>
-        (range 2)
-        (map #(hash-map :val % :user (random-uuid))))
-       :options
-       (->>
-        (range 3)
-        (map #(hash-map :val % :keys %)))
-       :room {:title "room title"}})
